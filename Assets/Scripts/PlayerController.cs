7e06e6438a72a23318528d8151527e647c043850
enigmaDevelopments <enigmarezac@gmail.com>
2025-10-02 14:51:15 -0400

    made player look in direction of movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,7 +5,6 @@ public class PlayerController : MonoBehaviour
     public float speed = 1.0f;
     private Rigidbody rb;
 
-
     void Start()
     {
         rb = GetComponent<Rigidbody>();
@@ -16,8 +15,14 @@ public class PlayerController : MonoBehaviour
     {
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
+        Debug.Log($"Horizontal: {horizontal}, Vertical: {vertical}");
         Vector3 movement = new Vector3(horizontal, 0.0f, vertical);
         movement = Vector3.ClampMagnitude(movement, 1) * speed;
         rb.linearVelocity = movement;
+        float rawHorizontal = Input.GetAxisRaw("Horizontal");
+        float rawVertical = Input.GetAxisRaw("Vertical");
+        Vector3 raw = new Vector3(rawHorizontal, 0.0f, rawVertical);
+        if (raw != Vector3.zero)
+            rb.MoveRotation(Quaternion.LookRotation(movement));
     }
 }
