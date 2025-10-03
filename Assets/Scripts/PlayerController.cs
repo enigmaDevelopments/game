0750a419fae9eaed0f1cf7da6cb923e03d57f163
enigmaDevelopments <enigmarezac@gmail.com>
2025-10-02 21:06:21 -0400

    integrated player controller script with Unity's Input System

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,28 +1,27 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
     public float speed = 1.0f;
+    private PlayerInput controles;
     private Rigidbody rb;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        controles = GetComponent<PlayerInput>();
+        controles.ActivateInput();
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
-        Debug.Log($"Horizontal: {horizontal}, Vertical: {vertical}");
-        Vector3 movement = new Vector3(horizontal, 0.0f, vertical);
-        movement = Vector3.ClampMagnitude(movement, 1) * speed;
-        rb.linearVelocity = movement;
-        float rawHorizontal = Input.GetAxisRaw("Horizontal");
-        float rawVertical = Input.GetAxisRaw("Vertical");
-        Vector3 raw = new Vector3(rawHorizontal, 0.0f, rawVertical);
-        if (raw != Vector3.zero)
-            rb.MoveRotation(Quaternion.LookRotation(movement));
+        Vector2 movement = controles.actions["Move"].ReadValue<Vector2>();
+        movement = Vector2.ClampMagnitude(movement, 1) * speed;
+        Vector3 movement3d = new Vector3(movement.x, 0.0f, movement.y);
+        rb.linearVelocity = movement3d;
+        if (movement != Vector2.zero)
+            rb.MoveRotation(Quaternion.LookRotation(movement3d));
     }
 }
