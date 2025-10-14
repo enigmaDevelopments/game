884093f83e759f7c0baf6d9c3fb4d10354a20c63
enigmaDevelopments <enigmarezac@gmail.com>
2025-10-14 19:39:46 -0400

    Created detection radius

--- Assets/Scripts/AI.cs
+++ Assets/Scripts/AI.cs
@@ -5,6 +5,7 @@ public class AI : MonoBehaviour
 {
     
     public float runAwayDistance;
+    public float detectionRadius;
     private NavMeshAgent agent;
     private Transform player;
     
@@ -18,8 +19,7 @@ public class AI : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        Vector3 destnation;
-            destnation = player.position + (transform.position - player.position).normalized * runAwayDistance;
-        agent.SetDestination(destnation);
+        if (Vector3.Distance(transform.position, player.position) < detectionRadius)
+            agent.SetDestination(player.position + runAwayDistance * (transform.position - player.position).normalized);
     }
 }
