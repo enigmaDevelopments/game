dbd49f7b9817b00d8cd6b59f62c26000a2f892b1
enigmaDevelopments <enigmarezac@gmail.com>
2025-10-15 09:50:11 -0400

    gave ability to hide veiw settings

--- Assets/Editor/AiEditorScript.cs
+++ Assets/Editor/AiEditorScript.cs
@@ -12,12 +12,11 @@ public class AiEditorScript : Editor
         Handles.color = Color.blue;
         Handles.DrawWireArc(ai.transform.position, Vector3.up, Vector3.forward, 360, ai.detectionRadius);
     }
-    
-   public override void OnInspectorGUI()
-   {
+
+    public override void OnInspectorGUI()
+    {
         AI ai = (AI)target;
         serializedObject.Update();
-        DrawPropertiesExcluding(serializedObject, "runAway", "runAwayRadius", "detection", "detectionRadius");
         ai.runAway = EditorGUILayout.Toggle("Run Away", ai.runAway);
         if (ai.runAway)
             ai.runAwayRadius = EditorGUILayout.FloatField("Run Away Radius", ai.runAwayRadius);
@@ -25,5 +24,16 @@ public class AiEditorScript : Editor
         if (ai.detection)
             ai.detectionRadius = EditorGUILayout.FloatField("Detection Radius", ai.detectionRadius);
         serializedObject.ApplyModifiedProperties();
+
+        ai.seight = EditorGUILayout.Toggle("seight", ai.seight);
+        if (ai.seight)
+        {
+            ai.raycast = EditorGUILayout.Toggle("Raycast", ai.raycast);
+            if (ai.raycast)
+                ai.enviromentMask = EditorGUILayout.LayerField("Enviroment Mask", ai.enviromentMask);
+            ai.veiwRadius = EditorGUILayout.FloatField("Veiw Radius", ai.veiwRadius);
+            ai.veiwAngle = EditorGUILayout.Slider("Veiw Angle", ai.veiwAngle, 0, 360);
+        }
+        DrawPropertiesExcluding(serializedObject, "m_Script", "runAway", "runAwayRadius", "detection", "detectionRadius", "seight", "veiwAngle", "veiwRadius", "raycast", "enviromentMask");
     }
 }
