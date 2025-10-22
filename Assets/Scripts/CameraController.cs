69df37395fd1b76aa64e134ea692c9ed1d10bcff
enigmaDevelopments <enigmarezac@gmail.com>
2025-10-22 14:02:54 -0400

    move camera to wall

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -3,8 +3,12 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
+    public LayerMask enviromentMask;
     public float upSpeed;
     public float downSpeed;
+    public float standeredRaduis;
+    public float middleRaduis;
+    public float closestRaduis;
     private CinemachineOrbitalFollow follower;
     private Transform target;
 
@@ -17,5 +21,15 @@ public class CameraController : MonoBehaviour
     void FixedUpdate()
     {
         follower.TrackerSettings.PositionDamping.y = transform.position.y - (Mathf.Tan(follower.VerticalAxis.Value * Mathf.Deg2Rad) * follower.Radius) <= target.position.y ? upSpeed : downSpeed;
+    }
+    private void Update()
+    {
+        if (Physics.Linecast(target.position, transform.position, out RaycastHit hit, enviromentMask))
+        {
+            if (middleRaduis < hit.distance)
+                follower.Radius = hit.distance;
+        }
+        else
+            follower.Radius = standeredRaduis;
     }
 }
