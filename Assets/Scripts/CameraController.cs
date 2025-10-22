ab8c0b0e00e92c0383225fa399694e8d2af275ac
enigmaDevelopments <enigmarezac@gmail.com>
2025-10-22 17:34:07 -0400

    let camera move vertically

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,5 +1,6 @@
 using Unity.Cinemachine;
 using UnityEngine;
+using UnityEngine.Rendering;
 using UnityEngine.Splines;
 
 public class CameraController : MonoBehaviour
@@ -27,32 +28,51 @@ public class CameraController : MonoBehaviour
     }
     private void Update()
     {
-        float distance = Mathf.Max(Vector3.Distance(transform.position, target.position), standeredRaduis);
-        Vector3 direction = (transform.position - target.position).normalized;
-
-        if (Physics.Raycast(new Ray(target.position,direction),out RaycastHit hit, distance, enviromentMask))
+        Vector3 origin = transform.position + Quaternion.Euler(follower.VerticalAxis.Value, follower.HorizontalAxis.Value, 0) * Vector3.forward * follower.Radius;
+        Vector3 direction = Quaternion.Euler(follower.VerticalAxis.Center, follower.HorizontalAxis.Value, 0) * Vector3.back;
+        Debug.DrawLine(target.position, transform.position, Color.red);
+        Debug.DrawRay(origin, direction * standeredRaduis, Color.blue);
+        if (Physics.Linecast(target.position,transform.position) || Physics.Raycast(origin,direction, standeredRaduis))
         {
-            if (minimumRaduis < hit.distance)
-                follower.Radius = hit.distance;
-            //else
-            //{
-                
-            //}
+            float bestAngle = -1;
+            float bestDistance = float.NegativeInfinity;
+
+            for (float i = follower.VerticalAxis.Range.x; i < follower.VerticalAxis.Range.y; i += detectionSteps)
+            {
+                Vector3 newPosition = origin + Quaternion.Euler(i, follower.HorizontalAxis.Value, 0) * Vector3.back * standeredRaduis;
+                Debug.DrawLine(target.position, newPosition, Color.green);
+                if (Physics.Linecast(target.position,newPosition,out RaycastHit hit,enviromentMask))
+                {
+                    if (bestDistance < hit.distance)
+                    {
+                        if (standeredRaduis <= hit.distance)
+                        {
+                            bestDistance = minimumRaduis;
+                            bestAngle = i;
+                            break;
+                        }
+                        bestDistance = hit.distance;
+                        bestAngle = i;
+                    }
+                }
+                else
+                {
+                    bestDistance = standeredRaduis;
+                    bestAngle = i;
+                    break;
+                }
+            }
+            if (minimumRaduis < bestDistance)
+            {
+                follower.Radius = bestDistance;
+                follower.VerticalAxis.Value = bestAngle;
+            }
         }
         else
-            follower.Radius = standeredRaduis;
-
-
-        Vector3 origin = transform.position + Quaternion.Euler(follower.VerticalAxis.Value, follower.HorizontalAxis.Value, 0) * Vector3.forward * standeredRaduis;
-        Debug.DrawLine(transform.position, origin, Color.red);
-        for (float i = follower.VerticalAxis.Range.x; i < follower.VerticalAxis.Range.y; i += detectionSteps)
         {
-            Vector3 newPosition = origin + Quaternion.Euler(i, follower.HorizontalAxis.Value, 0) * Vector3.back * standeredRaduis;
-            Vector3 newDirection = (target.position - newPosition).normalized;
-
-            Debug.DrawRay(newPosition, newDirection * distance, Color.green);
-            Ray ray = new Ray(newPosition, newDirection);
-
+            follower.Radius = standeredRaduis;
+            follower.VerticalAxis.Value = follower.VerticalAxis.Center;
         }
+        
     }
 }
