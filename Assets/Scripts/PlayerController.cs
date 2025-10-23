729fa2c52809804af0126101af3eb04aef694d6e
enigmaDevelopments <enigmarezac@gmail.com>
2025-10-23 10:07:59 -0400

    rotate movement by camera angle

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,6 +3,7 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    public new Transform camera;
     public float speed = 1.0f;
     private PlayerInput controles;
     private Rigidbody rb;
@@ -20,6 +21,12 @@ public class PlayerController : MonoBehaviour
         Vector2 movement = controles.actions["Move"].ReadValue<Vector2>();
         movement = Vector2.ClampMagnitude(movement, 1) * speed;
         Vector3 movement3d = new Vector3(movement.x, 0.0f, movement.y);
+        // Adjust movement direction based on camera orientation
+        Vector3 cameraDirection = (transform.position - camera.position);
+        cameraDirection.y = 0;
+        cameraDirection.Normalize();
+        movement3d = Quaternion.LookRotation(cameraDirection) * movement3d;
+
         rb.linearVelocity = movement3d;
         if (movement != Vector2.zero)
             rb.MoveRotation(Quaternion.LookRotation(movement3d));
