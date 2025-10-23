825d6e5d3e4121f9291fbf65bbb62a90e728b77b
enigmaDevelopments <enigmarezac@gmail.com>
2025-10-23 11:14:23 -0400

    stop camera from moving some where to close

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -36,18 +36,44 @@ public class CameraController : MonoBehaviour
     }
     private void Update()
     {
-        origin = transform.position + Quaternion.Euler(follower.VerticalAxis.Value, follower.HorizontalAxis.Value, 0) * Vector3.forward * follower.Radius;
+        //origin = transform.position + Quaternion.Euler(follower.VerticalAxis.Value, follower.HorizontalAxis.Value, 0) * Vector3.forward * follower.Radius;
+        origin = target.position;
         Vector3 direction = Quaternion.Euler(follower.VerticalAxis.Center, follower.HorizontalAxis.Value, 0) * Vector3.back;
         Debug.DrawLine(target.position, transform.position, Color.red);
         Debug.DrawRay(origin, direction * standeredRaduis, Color.blue);
         if (Physics.Linecast(target.position,transform.position) || Physics.Raycast(origin,direction, standeredRaduis))
         {
             AngleData best = BestAngle(follower.HorizontalAxis.Value);
-            Debug.Log(best.angle + " : " + best.distance);
             if (minimumRaduis < best.distance)
             {
                 follower.Radius = best.distance;
                 follower.VerticalAxis.Value = best.angle;
+            }
+            else
+            {
+                float startAngle = follower.HorizontalAxis.Value;
+                Debug.Log(startAngle);
+                for (float i = 0; i < 180; i += 5)
+                {
+                    float angle = ((startAngle + i + 180) % 360 + 360) % 360 - 180;
+                    AngleData sideBest = BestAngle(angle);
+                    if (minimumRaduis < sideBest.distance)
+                    {
+                        follower.Radius = sideBest.distance;
+                        follower.VerticalAxis.Value = sideBest.angle;
+                        follower.HorizontalAxis.Value = angle;
+                        return;
+                    }
+                    angle = ((startAngle - i + 180) % 360 + 360) % 360 - 180;
+                    sideBest = BestAngle(angle);
+                    if (minimumRaduis < sideBest.distance)
+                    {
+                        follower.Radius = sideBest.distance;
+                        follower.VerticalAxis.Value = sideBest.angle;
+                        follower.HorizontalAxis.Value = angle;
+                        return;
+                    }
+                }
             }
         }
         else
