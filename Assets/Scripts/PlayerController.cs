b5343f45abe142275a57283caf084b763ebd94b5
enigmaDevelopments <167920573+enigmaDevelopments@users.noreply.github.com>
2025-10-26 19:52:36 -0400

    Revert "Scrum 12 camera controls"

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,7 +3,6 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    public new Transform camera;
     public float speed = 1.0f;
     private PlayerInput controles;
     private Rigidbody rb;
@@ -13,7 +12,6 @@ public class PlayerController : MonoBehaviour
         rb = GetComponent<Rigidbody>();
         controles = GetComponent<PlayerInput>();
         controles.ActivateInput();
-        Cursor.lockState = CursorLockMode.Locked;
     }
 
     // Update is called once per frame
@@ -22,12 +20,6 @@ public class PlayerController : MonoBehaviour
         Vector2 movement = controles.actions["Move"].ReadValue<Vector2>();
         movement = Vector2.ClampMagnitude(movement, 1) * speed;
         Vector3 movement3d = new Vector3(movement.x, 0.0f, movement.y);
-        // Adjust movement direction based on camera orientation
-        Vector3 cameraDirection = (transform.position - camera.position);
-        cameraDirection.y = 0;
-        cameraDirection.Normalize();
-        movement3d = Quaternion.LookRotation(cameraDirection) * movement3d;
-
         rb.linearVelocity = movement3d;
         if (movement != Vector2.zero)
             rb.MoveRotation(Quaternion.LookRotation(movement3d));
