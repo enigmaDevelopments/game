710713325ce6e1141baa59678eac5f54f4771908
enigmaDevelopments <enigmarezac@gmail.com>
2025-10-30 12:32:22 -0400

    made shotgun for block fist

--- Assets/Scripts/LaunchProjectile.cs
+++ Assets/Scripts/LaunchProjectile.cs
@@ -7,23 +7,31 @@ public class LaunchProjectile : AttackBase
     [Header("Projectile Settings")]
     public Rigidbody projectile;
     public float speed = 4f;
-    public float duration; 
+    public float duration;
     public Vector3 spawnOffset = new Vector3(0f, 0f, 0.5f);
 
     protected override IEnumerator ExecuteAttack()
     {
         if (projectile == null)
             yield break;
-
-        Rigidbody p = Instantiate(projectile, transform.position + transform.TransformDirection(spawnOffset), transform.rotation);
-        p.linearVelocity = transform.forward * speed;
-        Projectile projectileScript = p.GetComponent<Projectile>();
-        projectileScript.duration = duration;
-        projectileScript.owner = gameObject;
-
+        Shoot();
         // No additional timing needed; projectile is fired instantly.
         yield break;
     }
+
+    protected virtual void Shoot()
+    {
+        Shoot(Quaternion.identity);
+    }
+
+    protected void Shoot(Quaternion rotation)
+    {
+        Rigidbody p = Instantiate(projectile, transform.position + transform.TransformDirection(spawnOffset), transform.rotation);
+        p.linearVelocity = rotation * transform.forward * speed;
+        Projectile projectileScript = p.GetComponent<Projectile>();
+        projectileScript.duration = duration;
+        projectileScript.owner = gameObject;
+    } 
 }
 
 
