ca68b06b0dabd22e1b08473ab65ca2a331cd4309
enigmaDevelopments <enigmarezac@gmail.com>
2025-11-03 11:42:26 -0500

    made particle system for pulse pack

---
+++ Assets/Scripts/PulseCreator.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using UnityEngine;
+
+public class PulseCreator : AttackBase
+{
+    protected override IEnumerator ExecuteAttack()
+    {
+        yield break;
+    }
+}
