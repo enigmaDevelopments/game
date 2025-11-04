9c70a575ca363deb105c84c38a08bce4b05946c6
enigmaDevelopments <enigmarezac@gmail.com>
2025-11-04 16:08:50 -0500

    make laser turn off

--- Assets/Scripts/Laser.cs
+++ Assets/Scripts/Laser.cs
@@ -4,13 +4,12 @@ using UnityEngine;
 
 public class Laser : AttackBase
 {
+    [Header("Laser settings")]
     public LineRenderer lineRenderer;
     public float raduis;
 
-    private void Start()
-    {
-        lineRenderer.endWidth = raduis;
-    }
+    private ulong timeStep = 0;
+
 
     protected override IEnumerator ExecuteAttack()
     {
@@ -18,10 +17,24 @@ public class Laser : AttackBase
         if (Physics.SphereCast(transform.position, raduis, transform.forward, out RaycastHit hit))
         {
             lineRenderer.positionCount = 2;
+            lineRenderer.endWidth = raduis*2;
             lineRenderer.SetPositions(new Vector3[] { transform.position, hit.point + hit.normal * raduis });
+            timeStep++;
+            StartCoroutine(KillLaser(timeStep));
         }
         else
             lineRenderer.positionCount = 0;
+        yield break;
+    }
+    private IEnumerator KillLaser(ulong time)
+    {
+        yield return null;
+        yield return new WaitForEndOfFrame();
+        if (time == timeStep)
+        {
+            lineRenderer.positionCount = 0;
+            timeStep = 0;
+        }
         yield break;
     }
 }
