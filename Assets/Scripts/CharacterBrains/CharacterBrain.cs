411d7378a7ca5dec472fbf3a44bd7b1c45ae71ab
enigmaDevelopments <enigmarezac@gmail.com>
2025-11-04 19:29:03 -0500

    made brain work with attack controller

--- Assets/Scripts/CharacterBrains/CharacterBrain.cs
+++ Assets/Scripts/CharacterBrains/CharacterBrain.cs
@@ -2,7 +2,10 @@ using UnityEngine;
 
 public class CharacterBrain : MonoBehaviour
 {
+    public AttackController attackController;
+    [Header("Settings")]
     public bool hasMeleeWeapon;
     public bool hasProjectileWeapon;
-    public bool canDash;
+    public bool hasSpecial;
+    public bool hasDash;
 }
