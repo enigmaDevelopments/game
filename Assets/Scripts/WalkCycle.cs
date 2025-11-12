61981209628380bab3940f61ea39f9cb07f1fd79
enigmaDevelopments <enigmarezac@gmail.com>
2025-11-12 13:45:57 -0500

    made idle animation

--- Assets/Scripts/WalkCycle.cs
+++ Assets/Scripts/WalkCycle.cs
@@ -1,3 +1,5 @@
+using System.Threading;
+using UnityEditor.ShaderGraph.Internal;
 using UnityEngine;
 
 public class WalkCycle : MonoBehaviour
@@ -10,21 +12,49 @@ public class WalkCycle : MonoBehaviour
     public float footDistence;
     public float stepHeight;
     public float stepDistence;
+    public float idleTime;
 
     private Vector3 lastPosition;
     private float totalDistence = 0;
-    void Start()
-    {
-        
-    }
+    private float idleTimer = 0;
+    private float leftReturnTimer = -1;
+    private float rightReturnTimer = -1;
+    
 
-    // Update is called once per frame
     void Update()
     {
-        totalDistence += Vector3.Distance(transform.position, lastPosition)/stepDistence;
+        float distance = Vector3.Distance(transform.position, lastPosition) / stepDistence;
+        totalDistence += distance;
         lastPosition = transform.position;
-        Vector3 leftFootPosition = new Vector3(footDistence, stepHeight * verticalCurive.Evaluate(totalDistence), stepDistence * horizontalCurive.Evaluate(totalDistence));
-        Vector3 rightFootPosition = new Vector3(-footDistence, stepHeight * verticalCurive.Evaluate(totalDistence-.5f), stepDistence * horizontalCurive.Evaluate(totalDistence - .5f));
+        if (0 < distance)
+            idleTimer = idleTime;
+        else
+            idleTimer -= Time.deltaTime;
+        Vector3 leftFootPosition;
+        Vector3 rightFootPosition;
+        #region walk cycle
+        if (0 < idleTimer) {
+            leftFootPosition = new Vector3(footDistence, stepHeight * verticalCurive.Evaluate(totalDistence), stepDistence * horizontalCurive.Evaluate(totalDistence));
+            rightFootPosition = new Vector3(-footDistence, stepHeight * verticalCurive.Evaluate(totalDistence - .5f), stepDistence * horizontalCurive.Evaluate(totalDistence - .5f));
+            leftReturnTimer = -1;
+        }
+        #endregion
+        #region idle
+        else
+        {
+            if (leftReturnTimer == -1)
+            {
+                leftReturnTimer = totalDistence % 1;
+                rightReturnTimer = (totalDistence - .5f) % 1;
+            }
+            leftReturnTimer = Mathf.Clamp01(leftReturnTimer + Time.deltaTime * (leftReturnTimer < .5? -1:1));
+            rightReturnTimer = Mathf.Clamp01(rightReturnTimer + Time.deltaTime * (rightReturnTimer < .5 ? -1 : 1));
+            leftFootPosition = new Vector3(footDistence, stepHeight * verticalCurive.Evaluate(leftReturnTimer), stepDistence * horizontalCurive.Evaluate(leftReturnTimer));
+            rightFootPosition = new Vector3(-footDistence, stepHeight * verticalCurive.Evaluate(rightReturnTimer), stepDistence * horizontalCurive.Evaluate(rightReturnTimer));
+
+            totalDistence = 0;
+        }
+        #endregion
         leftFoot.localPosition = leftFootPosition;
         rightFoot.localPosition = rightFootPosition;
     }
