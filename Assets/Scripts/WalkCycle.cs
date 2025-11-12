89b9590c855f0464722126b73ba94ec7bf4234f7
enigmaDevelopments <enigmarezac@gmail.com>
2025-11-12 12:58:18 -0500

    made walk cycle based off movement

--- Assets/Scripts/WalkCycle.cs
+++ Assets/Scripts/WalkCycle.cs
@@ -10,6 +10,9 @@ public class WalkCycle : MonoBehaviour
     public float footDistence;
     public float stepHeight;
     public float stepDistence;
+
+    private Vector3 lastPosition;
+    private float totalDistence = 0;
     void Start()
     {
         
@@ -18,8 +21,10 @@ public class WalkCycle : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector3 leftFootPosition = new Vector3(footDistence, stepHeight * verticalCurive.Evaluate(Time.time), stepDistence * horizontalCurive.Evaluate(Time.time));
-        Vector3 rightFootPosition = new Vector3(-footDistence, stepHeight * verticalCurive.Evaluate(Time.time-.5f), stepDistence * horizontalCurive.Evaluate(Time.time-.5f));
+        totalDistence += Vector3.Distance(transform.position, lastPosition)/stepDistence;
+        lastPosition = transform.position;
+        Vector3 leftFootPosition = new Vector3(footDistence, stepHeight * verticalCurive.Evaluate(totalDistence), stepDistence * horizontalCurive.Evaluate(totalDistence));
+        Vector3 rightFootPosition = new Vector3(-footDistence, stepHeight * verticalCurive.Evaluate(totalDistence-.5f), stepDistence * horizontalCurive.Evaluate(totalDistence - .5f));
         leftFoot.localPosition = leftFootPosition;
         rightFoot.localPosition = rightFootPosition;
     }
