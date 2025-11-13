c4d1950c97c6c4573579d2ad48bfa02099487ef2
enigmaDevelopments <enigmarezac@gmail.com>
2025-11-13 14:42:16 -0500

    made foot stay on ground

--- Assets/Scripts/WalkCycle.cs
+++ Assets/Scripts/WalkCycle.cs
@@ -1,18 +1,28 @@
 using System.Threading;
 using UnityEditor.ShaderGraph.Internal;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class WalkCycle : MonoBehaviour
 {
+    [Header("Transforms")]
     public Transform leftFoot;
     public Transform rightFoot;
+    public Transform feetRoot;
 
+    [Header("Masks")]
+    public LayerMask enviromentMask;
+
+    [Header("Animation curves")]
     public AnimationCurve verticalCurive;
     public AnimationCurve horizontalCurive;
+
+    [Header("Walk Settings")]
     public float footDistence;
     public float stepHeight;
     public float stepDistence;
     public float idleTime;
+    public float footHeight;
 
     private Vector3 lastPosition;
     private float totalDistence = 0;
@@ -54,6 +64,23 @@ public class WalkCycle : MonoBehaviour
 
             totalDistence = 0;
         }
+        #endregion
+        #region Keep feet on ground
+        Vector3 leftFootMax = leftFootPosition;
+        Vector3 rightFootMax = rightFootPosition;
+        leftFootMax.y = rightFootMax.y = stepHeight;
+        leftFootMax = feetRoot.TransformPoint(leftFootMax);
+        rightFootMax = feetRoot.TransformPoint(rightFootMax);
+        RaycastHit leftHitInfo;
+        RaycastHit rightHitInfo;
+        Physics.Raycast(leftFootMax, Vector3.down, out leftHitInfo,float.PositiveInfinity, enviromentMask);
+        Physics.Raycast(rightFootMax, Vector3.down, out rightHitInfo, float.PositiveInfinity, enviromentMask);
+        float leftFootMin = stepHeight + footHeight - leftHitInfo.distance;
+        float rightFootMin = stepHeight + footHeight - rightHitInfo.distance;
+        Debug.DrawRay(leftFootMax, Vector3.down*100,Color.blue);
+        Debug.DrawRay(rightFootMax, Vector3.down * 100, Color.blue);
+        leftFootPosition.y = Mathf.Clamp(leftFootPosition.y, leftFootMin, stepHeight);
+        rightFootPosition.y = Mathf.Clamp(rightFootPosition.y, rightFootMin, stepHeight);
         #endregion
         leftFoot.localPosition = leftFootPosition;
         rightFoot.localPosition = rightFootPosition;
