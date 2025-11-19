46ca839610955b6680e59be22a6010461f57aa4a
enigmaDevelopments <enigmarezac@gmail.com>
2025-11-18 23:48:40 -0500

    made weapon swing

--- Assets/Scripts/WeponSwing.cs
+++ Assets/Scripts/WeponSwing.cs
@@ -1,28 +1,57 @@
+using Unity.Mathematics;
 using UnityEngine;
 
 public class WeponSwing : MonoBehaviour
 {
     public Transform arm;
     public Transform sholder;
+    public quaternion maxSwingAngle;
     public float loadSeconds;
     public float swingSeconds;
     public bool canSwing;
+    public bool swing;
 
     Quaternion start = Quaternion.identity;
     Quaternion armRotation = Quaternion.Euler(0, 90, 0);
     Quaternion sholderRotation = Quaternion.Euler(0, 0, -90);
-    float timer = 0f;
+    float loadTimer = 0;
+    float swingTimer = 0;
+    private bool reverseSwing = false;
 
     // Update is called once per frame
     void Update()
     {
         if (canSwing)
-            timer += Time.deltaTime / loadSeconds;
+        {
+            loadTimer += Time.deltaTime / loadSeconds;
+            if (swing)
+            {
+                if (reverseSwing)
+                    swingTimer -= Time.deltaTime / swingSeconds;
+                else
+                    swingTimer += Time.deltaTime / swingSeconds;
+            }
+            if(1 <= swingTimer)
+                reverseSwing = true;
+
+            if (swingTimer <= 0)
+            {
+                reverseSwing = false;
+                swing = false;
+            }
+        }
         else
-            timer -= Time.deltaTime / loadSeconds;
-        timer = Mathf.Clamp01(timer);
-        arm.localRotation = Quaternion.Slerp(start, armRotation, timer);
-        sholder.localRotation = Quaternion.Slerp(start, sholderRotation, timer);
+        {
+            loadTimer -= Time.deltaTime / loadSeconds;
+            swingTimer = 0;
+        }
+        loadTimer = Mathf.Clamp01(loadTimer);
+        arm.localRotation = Quaternion.Slerp(start, armRotation, loadTimer);
+        if (swing)
+            sholder.localRotation = Quaternion.Slerp(sholderRotation, maxSwingAngle, swingTimer);
+        else
+            sholder.localRotation = Quaternion.Slerp(start, sholderRotation, loadTimer);
+        
 
     }
 }
