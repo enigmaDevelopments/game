a4e33cdd883ddfef413b95b90d49ecb4c88c650a
enigmaDevelopments <enigmarezac@gmail.com>
2025-12-06 19:36:19 -0500

    mele does damage

--- Assets/Scripts/Attacks/meleAttack.cs
+++ Assets/Scripts/Attacks/meleAttack.cs
@@ -3,10 +3,22 @@ using System.Collections;
 
 public class meleAttack : AttackBase
 {
+    private bool attacking;
+    private int id;
     protected override IEnumerator ExecuteAttack()
     {
+        attacking = true;
+        id = Random.Range(int.MinValue, int.MaxValue);
         yield return StartCoroutine(animation.attackingStance());
         yield return StartCoroutine(animation.Attack());
+        attacking = false;
         yield break;
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        GameObject attacked = other.transform.root.gameObject;
+        Health health = attacked.GetComponent<Health>();
+        if (health != null && attacking)
+            health.TakeDamage(damage, id);
     }
 }
