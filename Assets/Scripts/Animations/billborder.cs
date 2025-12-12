df8166e5a2206c99a46700e960f0483827d38761
enigmaDevelopments <enigmarezac@gmail.com>
2025-12-12 15:07:20 -0500

    removed unneeded billboarding script

--- Assets/Scripts/Animations/billborder.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public class billborder : MonoBehaviour
-{
-    private void LateUpdate()
-    {
-        transform.forward = (Camera.main.transform.position - transform.position).normalized;
-    }
-}
