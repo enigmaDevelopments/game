5aec4d94a35b9651b4bdc9b2c7b3b9c7e7f46f1e
hessink1 <89397792+hessink1@users.noreply.github.com>
2025-12-12 23:20:23 -0700

    seeker brain

--- Assets/Scripts/CharacterBrains/SeekerBrain.cs
+++ Assets/Scripts/CharacterBrains/SeekerBrain.cs
@@ -1,6 +1,77 @@
 using UnityEngine;
+using static AntennaBrain;
 
 public class SeekerBrain : CharacterBrain
 {
+    public enum Weapons : byte
+    {
+        None,
+        invisabilityChip,
+        grapplingHook
+    }
+    public Weapons defaultWeapons;
+
+    public GameObject invisabilityChipObject;
+    public GameObject grapplingHookObject;
+
+    private bool _invisabilityChip = false;
+    private bool _grapplingHook = false;
+
+    public bool invisabilityChip
+    {
+        get { return _invisabilityChip; }
+        set
+        {
+            if (value)
+            {
+                _grapplingHook = false;
+           
+                SetSpecialWepon(invisabilityChipObject);
+            }
+            else if (_invisabilityChip)
+            {
+                RemoveSpecialWepon();
+            }
+            _invisabilityChip = value;
+        }
+    }
+
+    public bool grapplingHook
+    {
+        get { return _grapplingHook; }
+        set
+        {
+            if (value)
+            {
+                invisabilityChip = false;
+               
+                SetSpecialWepon(grapplingHookObject);
+            }
+            else if (_grapplingHook)
+            {
+                RemoveSpecialWepon();
+            }
+            _grapplingHook = value;
+        }
+    }
+
+    public void RemoveWeapon()
+    {
+        _invisabilityChip = false;
+        _grapplingHook = false;
+        
+        RemoveSpecialWepon();
+    }
+
+    protected override void Awake()
+    {
+        base.Awake();
+        if (defaultWeapons == Weapons.invisabilityChip)
+            invisabilityChip = true;
+        else if (defaultWeapons == Weapons.grapplingHook)
+            grapplingHook = true;
+       
+    }
+
 
 }
