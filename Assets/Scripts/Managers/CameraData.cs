cb871a40e1b596ce270e332a33dc6e90f7acaa34
enigmaDevelopments <enigmarezac@gmail.com>
2025-12-14 13:25:29 -0500

    auto set up cameras

--- Assets/Scripts/Managers/CameraData.cs
+++ Assets/Scripts/Managers/CameraData.cs
@@ -6,4 +6,10 @@ public class CameraData : MonoBehaviour
     public CinemachineCamera normalCamera;
     public CinemachineCamera aimCamera;
     public Canvas crosshairCanvas;
+
+    private void Start()
+    {
+        normalCamera.Target = new CameraTarget { TrackingTarget = GameObject.FindGameObjectWithTag("Player").transform };
+        aimCamera.Target = new CameraTarget { TrackingTarget = GameObject.FindGameObjectWithTag("Aim").transform };
+    }
 }
